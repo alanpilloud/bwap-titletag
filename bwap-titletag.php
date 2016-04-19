6def8f66d9d9d636c69a30b360425ea8becd51f3
alanpilloud <alanpilloud@gmail.com>
2016-04-19 22:16:15 +0200

    Add title management from bwap-titletag

--- bwap-titletag.php
+++ bwap-titletag.php
@@ -6,15 +6,26 @@ Description: Manages the title tag
 Version: 0.1
 */
 
-add_action('wp_head', function () {
-    /**
-    * Set title tag differently on the frontpage
-    */
+/**
+*   Define the title of the page
+*
+*   This is a named function because it might be used.
+*/
+function bwap_get_title() {
     if (is_front_page()) {
         $title = get_bloginfo('name');
     } else {
         $title = wp_title('|', false, 'right').get_bloginfo('name');
     }
-    echo '<title>'.$title.'</title>';
+
+    return $title;
+}
+
+add_action('wp_head', function () {
+    /**
+    * Set title tag differently on the frontpage
+    */
+
+    echo '<title>'.bwap_get_title().'</title>';
 
 }, 0);
